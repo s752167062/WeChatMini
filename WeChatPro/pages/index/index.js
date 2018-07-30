d7ff3ad1a8ead5fecac111be1eb2130cf8305c9b
s752167062 <752167062@qq.com>
2018-07-30 15:41:26 +0800

    小程序修改

--- WeChatPro/pages/index/index.js
+++ WeChatPro/pages/index/index.js
@@ -7,7 +7,15 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+    duration: 2000,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    loading: false,
+    plain: false,
+    lists:[],
   },
   //事件处理函数
   bindViewTap: function() {
@@ -35,6 +43,32 @@ Page({
       })
       app.globalData.userInfo = info;
     })
+  },
+
+  bindGetMsg:function(data){
+    console.log("data")
+    wx.showModal({
+      title: '提示',
+      content: data,
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+
+  },
+
+  add:function(){
+    console.log(">>> addBtn ");
+    var { lists } = this.data;
+    var newData = { url: "http://img.zcool.cn/community/01b88659bb7903a801212fb7949ee0.jpg@2o.jpg",name:"嘻嘻嘻嘻嘻" ,time:"2018-7-30 15:34"};
+    lists.push(newData);
+    this.setData({
+      lists: lists
+    }) 
   }
 
 })
